85603f63f8334dde78c86eaca4db8a31937b7c84
syarifmaulanamm <syarifmaulanamm@gmail.com>
2021-06-02 11:21:20 +0700

    JS Server

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+
+fs.readFile('./index.html', function ( err, html ) {
+    if ( err ) {
+        throw err;
+    }
+    
+    http.createServer( function ( req, res ) {
+        res.writeHead(200, { "Content-Type" : "text/html"});
+        res.write(html);
+        res.end();
+    }).listen(8080);
+});
