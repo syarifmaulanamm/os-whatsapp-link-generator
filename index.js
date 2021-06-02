b4f6cee8dfaefd1c42d84dcbbad221f369f5f064
Syarif Maulana <syarifmaulanamm@gmail.com>
2021-06-02 12:09:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ fs.readFile('./index.html', function ( err, html ) {
     }
     
     http.createServer( function ( req, res ) {
+        console.log('Server Up');
         res.writeHead(200, { "Content-Type" : "text/html"});
         res.write(html);
         res.end();
